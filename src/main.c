15af3f4909b14c769acc14f3744d253fce8f5cb3
Sebastian Pipping <sebastian@pipping.org>
2010-10-15 17:07:43 +0200

    Fix warning about mixed signed and unsigend

--- src/main.c
+++ src/main.c
@@ -126,7 +126,7 @@ int main(int argc, char **argv) {
 	if (CHECK_FLAG(dest.flags, FLAG_RANDOM)) {
 		unsigned int const seed = CHECK_FLAG(dest.flags, FLAG_USER_SEED)
 			? dest.seed
-			: time(NULL) + getpid();
+			: (unsigned int)(time(NULL) + getpid());
 		srand(seed);
 	}
 
