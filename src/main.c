5e180bf1515ff5cf018988510ebde2ad18fa3f41
Jan Hauke Rahm <jhr@debian.org>
2011-05-27 11:00:20 +0200

    Leave out parenthesis that are now set by macro

--- src/main.c
+++ src/main.c
@@ -102,7 +102,7 @@ int main(int argc, char **argv) {
 	enum_complete_scaffold(&dest);
 	assert(ENUM_IS_READY((&dest)));
 
-	if (ENUM_CHECK_FLAG(dest.flags, FLAG_EQUAL_WIDTH) && ! HAS_RIGHT((&dest))) {
+	if (ENUM_CHECK_FLAG(dest.flags, FLAG_EQUAL_WIDTH) && ! HAS_RIGHT(&dest)) {
 		fprintf(stderr, "Combining -e|--equal-width and infinity not supported.\n");
 		return 1;
 	}
